6545ae6a653c4bf1ab3cd6e2dc4d0357d5f99000
Michael S <user@mail.com>
2018-12-28 00:28:28 -0700

    Updates

---
+++ precache-manifest.a4992a425d55f2f6002fba42e04ec30c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d4874e785311d7f2181a479445dfdfc",
+    "url": "/ClickyGame/static/media/Guile.6d4874e7.png"
+  },
+  {
+    "revision": "cf048d7d8cc39a3e4574",
+    "url": "/ClickyGame/static/css/main.cce5696a.chunk.css"
+  },
+  {
+    "revision": "55a09e2a6ba07f6129d3",
+    "url": "/ClickyGame/static/js/1.55a09e2a.chunk.js"
+  },
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "222fcdaa075f9d1b3efafc81bf1cf0a9",
+    "url": "/ClickyGame/static/media/Balrog.222fcdaa.png"
+  },
+  {
+    "revision": "f605dedc2615498b440f970bab2491ee",
+    "url": "/ClickyGame/static/media/Blanka.f605dedc.png"
+  },
+  {
+    "revision": "fc3ce4ab953c8238d6b6dd271119f5ec",
+    "url": "/ClickyGame/static/media/ChunLi.fc3ce4ab.png"
+  },
+  {
+    "revision": "3b8562c7b81707df549cd1b34012048e",
+    "url": "/ClickyGame/static/media/Dhalsim.3b8562c7.png"
+  },
+  {
+    "revision": "5f7e83e74ec9baa4052ebd89d3d2498f",
+    "url": "/ClickyGame/static/media/Ehonda.5f7e83e7.png"
+  },
+  {
+    "revision": "cf048d7d8cc39a3e4574",
+    "url": "/ClickyGame/static/js/main.cf048d7d.chunk.js"
+  },
+  {
+    "revision": "9d40fc9d0ca92badd8d3726133e08b61",
+    "url": "/ClickyGame/static/media/Ken.9d40fc9d.png"
+  },
+  {
+    "revision": "a295bc53a53d5b0e657059b57905ca97",
+    "url": "/ClickyGame/static/media/MBison.a295bc53.png"
+  },
+  {
+    "revision": "5fdf4e54a34b224373f08873b4009d52",
+    "url": "/ClickyGame/static/media/Ryu.5fdf4e54.png"
+  },
+  {
+    "revision": "79c1e1b532949a864d3cf2a825db9690",
+    "url": "/ClickyGame/static/media/Sagat.79c1e1b5.png"
+  },
+  {
+    "revision": "986d487531165f69181c85119f313ae7",
+    "url": "/ClickyGame/static/media/Vega.986d4875.png"
+  },
+  {
+    "revision": "271431ae6f25d2c1e3b3630a381afd50",
+    "url": "/ClickyGame/static/media/Zangief.271431ae.png"
+  },
+  {
+    "revision": "48c6a9d6a356ce65f1ca8f3c2beae7bb",
+    "url": "/ClickyGame/static/media/bg.48c6a9d6.png"
+  },
+  {
+    "revision": "587d91ad47436e7ae26639644b3651b5",
+    "url": "/ClickyGame/index.html"
+  }
+];
